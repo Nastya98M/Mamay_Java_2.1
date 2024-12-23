b0937dbcae71b406256790112900d7f5b444470a
System Administrator <root@MacBook-Air-andrey-2.local>
2024-12-23 17:44:29 +0300

    move to one repo

--- src/Task2_1.java
+++ src/Task2_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Task2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое целое число (a): ");
